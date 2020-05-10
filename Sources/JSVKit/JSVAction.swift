7773068ac3f9ef46cec063b08a8cc33699b19150
Jan <jsvensson51@yahoo.com>
2020-05-10 21:35:14 +0200

    Make JSVAction public

--- Sources/JSVKit/JSVAction.swift
+++ Sources/JSVKit/JSVAction.swift
@@ -1,18 +1,18 @@
 import ARKit
 
 /// An action that can be executed in an ARSceneViewController. Action is a super-class and should be implemented by another class.
-internal class JSVAction: NSObject, NSSecureCoding {
+public class JSVAction: NSObject, NSSecureCoding {
     
-    internal var executeEverywhere: Bool = true
+    public var executeEverywhere: Bool = true
 
-    internal class var supportsSecureCoding: Bool {
+    public class var supportsSecureCoding: Bool {
         return true
     }
 
-    internal func encode(with aCoder: NSCoder) {
+    public func encode(with aCoder: NSCoder) {
     }
 
-    required internal init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
     }
     
     /// Execute the action in a ARSceneViewController
@@ -20,7 +20,7 @@ internal class JSVAction: NSObject, NSSecureCoding {
     ///   - controller: The controller to be executed in
     ///   - callerID: The ID of the caller of the action
     ///   - completionHandler: A completion handler to be executed after the action has completed
-    internal func execute(in controller: UIViewController, callerID: String? = nil, completionHandler: (() -> Void)?) {
+    public func execute(in controller: UIViewController, callerID: String? = nil, completionHandler: (() -> Void)?) {
         fatalError("Execute function has not been implemented!")
     }
 }
