e3779bad50ea23877378617081da688280b1b275
Jan <jsvensson51@yahoo.com>
2022-02-26 16:12:18 +0100

    Add ability to ignore cache on UIImageView load

--- Sources/JSVKit/ImageCache/JSVImageCache.swift
+++ Sources/JSVKit/ImageCache/JSVImageCache.swift
@@ -1,16 +1,10 @@
-//
-//  ImageCache.swift
-//
-//  Created by Jan Svensson on 2021-08-19.
-//
-
 import Foundation
 import class UIKit.UIImage
 
 /// A class that is able to store images according to their URL
 public class ImageCache {
     /// Singleton object for the image cache
-    static var defaults: ImageCache = ImageCache()
+    public static var defaults: ImageCache = ImageCache()
 
     /// The cache map that holds the images
     var cache: [URL: UIImage]
--- Sources/JSVKit/ImageCache/UIImageView+load.swift
+++ Sources/JSVKit/ImageCache/UIImageView+load.swift
@@ -1,21 +1,19 @@
-//  UIImageView+load.swift
-//
-//  Created by Jan Svensson on 2021-08-19.
-//
-
 import UIKit
 
 extension UIImageView {
     /// Loads an image from a URL to the image view. Automatically caches the loaded image to the image cache.
     /// - Parameters:
     ///   - url: The URL to fetch from
+    ///   - useCache: Option whether to use cache or not
     ///   - completion: A completion function that is called when the image has been loaded
-    public func load(url: URL, _ completion: (() -> Void)? ) {
+    public func load(url: URL, useCache: Bool = true, _ completion: (() -> Void)? = nil) {
         let cache = ImageCache.defaults.cache
 
-        if let image = cache[url] {
-            self.image = image
-            return
+        if useCache {
+            if let image = cache[url] {
+                self.image = image
+                return
+            }
         }
 
         DispatchQueue.global(qos: .background).async { [weak self] in
